11a86d827b59f3e961572ffe3424d81fdbb19133
ubuntu-1404 <simul2013@126.com>
2015-09-09 22:02:14 +0800

    Update TDSS.java
    update for miss code

--- zy/TDSS.java
+++ zy/TDSS.java
@@ -14,15 +14,15 @@ public class TDSS{
 		if(this.tg.length > 2){
 			for(int i = 0;i < this.tg.length;i++){
 				if((this.dz[i] == 2 || this.dz[i] == 8) && (tg[2] == 1 || tg[2] == 5)){
-					res = "¼×Îì²¢Å£Ñò";
+					res = "ç”²æˆŠå¹¶ç‰›ç¾Š";
 				}else if((this.dz[i] == 1 || this.dz[i] == 9) && (tg[2] == 2 || tg[2] == 6)){
-					res = "ÒÒ¼ºÊóºïÏç";
+					res = "ä¹™å·±é¼ çŒ´ä¹¡";
 				}else if((this.dz[i] == 0 || this.dz[i] == 10) && (tg[2] == 3 || tg[2] == 4)){
-					res = "±û¶¡Öí¼¦Î»";
+					res = "ä¸™ä¸çŒªé¸¡ä½";
 				}else if((this.dz[i] == 4 || this.dz[i] == 6) && (tg[2] == 0 || tg[2] == 9)){
-					res = "ÈÉ¹ïÍÃÉß²Ø";
+					res = "å£¬ç™¸å…”è›‡è—";
 				}else if((this.dz[i] == 3 || this.dz[i] == 7) && (tg[2] == 7 || tg[2] == 8)){
-					res = "¸ýÐÁ·ê»¢Âí";
+					res = "åºšè¾›é€¢è™Žé©¬";
 				}
 			}
 		}
