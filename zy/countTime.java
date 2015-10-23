ed7bf9c9d479aa6c711f1067162085e59552265b
ubuntu-1404 <simul2013@126.com>
2015-10-23 17:51:21 +0800

    Update countTime.java
    update diff

--- zy/countTime.java
+++ zy/countTime.java
@@ -38,30 +38,30 @@ public class countTime{
     	}
     }
     private int Diff(String y1,String y2) {
-    	int res = 0;
-    	int temp1 = Integer.parseInt(y1.split("\\D")[0]);
-    	int temp2 = Integer.parseInt(y2.split("\\D")[0]);
-    	int temp3 = Integer.parseInt(y1.split("\\D")[1]);
-    	int temp4 = Integer.parseInt(y2.split("\\D")[1]);
-    	int temp5 = Integer.parseInt(y1.split("\\D")[2]);
-    	int temp6 = Integer.parseInt(y2.split("\\D")[2]);
-    	for (int i = 0;i<1+temp1-temp2;i++)//所有整年的总天数天数
+    	int		res = 0;
+    	int[]	tmp = new int[6];
+    	String[]	fory1 = y1.split("\\D"),fory2 = y2.split("\\D");
+    	for (int i = 0;i < 3;i++){
+    		tmp[i*2]   = Integer.parseInt(fory1[i]);
+    		tmp[i*2+1] = Integer.parseInt(fory2[i]);
+    	}
+    	for (int i = 0;i<1+tmp[0]-tmp[1];i++)//所有整年的总天数天数
         {
-            if((temp2+i)%100 == 0){
-            	if((temp2+i)%400 == 0){
-            		res = RN(i,temp1,temp2,temp3,temp4,temp5,temp6,res);
+            if((tmp[1]+i)%100 == 0){
+            	if((tmp[1]+i)%400 == 0){
+            		res = RN(i,tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5],res);
             	}else{
-            		res = FRN(i,temp1,temp2,temp3,temp4,temp5,temp6,res);
+            		res = FRN(i,tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5],res);
             	}
             }else{
-            	if((temp2+i)%4 == 0){
-            		res = RN(i,temp1,temp2,temp3,temp4,temp5,temp6,res);
+            	if((tmp[1]+i)%4 == 0){
+            		res = RN(i,tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5],res);
             	}else{
-            		res = FRN(i,temp1,temp2,temp3,temp4,temp5,temp6,res);
+            		res = FRN(i,tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5],res);
             	}
             }
         }
-		return res;
+	return res;
     }
 	private int RN(int i,int temp1, int temp2, int temp3, int temp4, int temp5,
 			int temp6,int res) {
@@ -101,60 +101,35 @@ public class countTime{
 		int			Y	= parseInt%100;
 		int[]		JQDay 	= {0,0};
 		double		D	= 0.2422;
-		double[]	C0	= {4.63,19.46,6.38,21.416,5.59,20.888,6.318,21.86,6.500,22.20,7.928,23.65,8.35,23.95,8.440,23.822,9.098,24.218,8.218,23.08,7.90,22.60,6.1100,20.84};
-		double[]	C1	= {3.87,18.74,5.63,20.646,4.81,20.100,5.520,21.04,5.678,21.37,7.108,22.83,7.50,23.13,7.646,23.042,8.318,23.438,7.438,22.36,7.18,21.94,5.4055,20.12};
-		double[]	C2	= {4.15,18.73,5.63,20.646,5.59,20.888,6.318,21.86,6.500,22.20,7.928,23.65,8.35,23.95,8.440,23.822,9.098,24.218,8.218,23.08,7.90,22.60,6.1100,20.84};
-		if (parseInt/100 == 19){
+		double[][]	Ce	= {
+				{4.63,19.46,6.38,21.416,5.59,20.888,6.318,21.86,6.500,22.20,7.928,23.65,8.35,23.95,8.440,23.822,9.098,24.218,8.218,23.08,7.90,22.60,6.1100,20.84},
+				{3.87,18.74,5.63,20.646,4.81,20.100,5.520,21.04,5.678,21.37,7.108,22.83,7.50,23.13,7.646,23.042,8.318,23.438,7.438,22.36,7.18,21.94,5.4055,20.12},
+				{4.15,18.73,5.63,20.646,5.59,20.888,6.318,21.86,6.500,22.20,7.928,23.65,8.35,23.95,8.440,23.822,9.098,24.218,8.218,23.08,7.90,22.60,6.1100,20.84}};
+		if (parseInt/100 >= 19){
 			if(i == 1 || i == 2){
 				if(Y == 0){
-					JQDay[0]	= (int) (Y*D+C0[(i*2+20)%24]);
-					JQDay[1]	= (int) (Y*D+C0[(i*2+21)%24]);
+					JQDay[0]	= (int) (Y*D+Ce[parseInt/100-19][(i*2+20)%24]);
+					JQDay[1]	= (int) (Y*D+Ce[parseInt/100-19][(i*2+21)%24]);
 				}else{
-					JQDay[0]	= (int) (Y*D+C0[(i*2+20)%24]-(Y-1)/4);
-					JQDay[1]	= (int) (Y*D+C0[(i*2+21)%24]-(Y-1)/4);
+					JQDay[0]	= (int) (Y*D+Ce[parseInt/100-19][(i*2+20)%24]-(Y-1)/4);
+					JQDay[1]	= (int) (Y*D+Ce[parseInt/100-19][(i*2+21)%24]-(Y-1)/4);
 				}
 			}else{
-				JQDay[0]	= (int) (Y*D+C0[(i*2+20)%24]-Y/4);
-				JQDay[1]	= (int) (Y*D+C0[(i*2+21)%24]-Y/4);
-			}
-		}else if (parseInt/100 == 20){
-			if(i == 1 || i == 2){
-				if(Y == 0){
-					JQDay[0]	= (int) (Y*D+C1[(i*2+20)%24]);
-					JQDay[1]	= (int) (Y*D+C1[(i*2+21)%24]);
-				}else{
-					JQDay[0]	= (int) (Y*D+C1[(i*2+20)%24]-(Y-1)/4);
-					JQDay[1]	= (int) (Y*D+C1[(i*2+21)%24]-(Y-1)/4);
-				}
-			}else{
-				JQDay[0]	= (int) (Y*D+C1[(i*2+20)%24]-Y/4);
-				JQDay[1]	= (int) (Y*D+C1[(i*2+21)%24]-Y/4);
-			}
-		}else if (parseInt/100 == 21){
-			if(i == 1 || i == 2){
-				if(Y == 0){
-					JQDay[0]	= (int) (Y*D+C2[(i*2+20)%24]);
-					JQDay[1]	= (int) (Y*D+C2[(i*2+21)%24]);
-				}else{
-					JQDay[0]	= (int) (Y*D+C2[(i*2+20)%24]-(Y-1)/4);
-					JQDay[1]	= (int) (Y*D+C2[(i*2+21)%24]-(Y-1)/4);
-				}
-			}else{
-				JQDay[0]	= (int) (Y*D+C2[(i*2+20)%24]-Y/4);
-				JQDay[1]	= (int) (Y*D+C2[(i*2+21)%24]-Y/4);
+				JQDay[0]	= (int) (Y*D+Ce[parseInt/100-19][(i*2+20)%24]-Y/4);
+				JQDay[1]	= (int) (Y*D+Ce[parseInt/100-19][(i*2+21)%24]-Y/4);
 			}
 		}else{
 			if(i == 1 || i == 2){
 				if(Y == 0){
-					JQDay[0]	= (int) (Y*D+C1[(i*2+20)%24]);
-					JQDay[1]	= (int) (Y*D+C1[(i*2+21)%24]);
+					JQDay[0]	= (int) (Y*D+Ce[1][(i*2+20)%24]);
+					JQDay[1]	= (int) (Y*D+Ce[1][(i*2+21)%24]);
 				}else{
-					JQDay[0]	= (int) (Y*D+C1[(i*2+20)%24]-(Y-1)/4);
-					JQDay[1]	= (int) (Y*D+C1[(i*2+21)%24]-(Y-1)/4);
+					JQDay[0]	= (int) (Y*D+Ce[1][(i*2+20)%24]-(Y-1)/4);
+					JQDay[1]	= (int) (Y*D+Ce[1][(i*2+21)%24]-(Y-1)/4);
 				}
 			}else{
-				JQDay[0]	= (int) (Y*D+C1[(i*2+20)%24]-Y/4);
-				JQDay[1]	= (int) (Y*D+C1[(i*2+21)%24]-Y/4);
+				JQDay[0]	= (int) (Y*D+Ce[1][(i*2+20)%24]-Y/4);
+				JQDay[1]	= (int) (Y*D+Ce[1][(i*2+21)%24]-Y/4);
 			}
 		}
 		System.out.println(parseInt+"-"+i+":"+JQDay[0]+"/"+JQDay[1]);
