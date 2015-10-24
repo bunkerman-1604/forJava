09116be1b0141200f61d162a37bc2f8e17b1b0e2
ubuntu-1404 <simul2013@126.com>
2015-10-24 21:48:43 +0800

    Update countTime.java
    fixed same year countday method bug for `not nature number`

--- zy/countTime.java
+++ zy/countTime.java
@@ -3,18 +3,37 @@ package backage;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.JOptionPane;
+
 
 public class countTime{
-    private static int[] ry={31,29,31,30,31,30,31,31,30,31,30,31};
-    private static int[] fry={31,28,31,30,31,30,31,31,30,31,30,31};
-    private String t1,t2;
+    private static int[]	ry	= {31,29,31,30,31,30,31,31,30,31,30,31};
+    private static int[]	fry	= {31,28,31,30,31,30,31,31,30,31,30,31};
+    private	int[]	temp;
     private Pattern p = Pattern.compile("\\d{4}(\\D\\d{1,2}){2,3}$");
     public countTime(String d1,String d2){
+    	String[]	tmp1,tmp2;
 		Matcher m1 = p.matcher(d1);
 		Matcher m2 = p.matcher(d2);
 		if(m1.find() && m2.find()){
-			this.t1 = m1.group();
-			this.t2 = m2.group();
+			tmp1	=	m1.group().split("\\D");
+			tmp2	=	m2.group().split("\\D");
+			if (tmp1.length	==	tmp2.length){
+				this.temp	=	new int[tmp1.length*2];
+				if (Integer.parseInt(tmp1[0]) > Integer.parseInt(tmp2[0])) {
+					for (int i = 0;i < tmp1.length;i++){
+						this.temp[i*2]		=	Integer.parseInt(tmp1[i]);
+						this.temp[i*2+1]	=	Integer.parseInt(tmp2[i]);
+					}
+				}else{
+					for (int i = 0;i < tmp1.length;i++){
+						this.temp[i*2]		=	Integer.parseInt(tmp2[i]);
+						this.temp[i*2+1]	=	Integer.parseInt(tmp1[i]);
+					}
+				}
+			}else{
+				JOptionPane.showMessageDialog(null, "it`s different length between the tow date String given !", "", JOptionPane.INFORMATION_MESSAGE);
+			}
 		}else{
 			System.out.print("构造countTime失败，参数无法匹配！d1="+d1+"d2="+d2);
 			System.exit(0);
@@ -22,46 +41,38 @@ public class countTime{
     }
     public countTime(String d1){
 		Matcher m = p.matcher(d1);
+		String[]	tmp;
 		if(m.find()){
-			this.t1 = m.group();
-			this.t2 = "1001-1-1";
+			tmp	=	m.group().split("\\D");
+			this.temp	=	new int[tmp.length*2];
+			for (int i = 0;i < tmp.length;i++){
+				this.temp[2*i]		=	Integer.parseInt(tmp[i]);
+			}
+			this.temp[1]	=	1001;this.temp[3]	=	1;this.temp[5]	=	1;
 		}else{
 			System.out.print("构造countTime失败，参数无法匹配！d1="+d1);
 			System.exit(0);
 		}
     }
-    public int getDiff(){
-    	if(Integer.parseInt(this.t1.split("\\D")[0])-Integer.parseInt(this.t2.split("\\D")[0]) > 0){
-    		return Diff(this.t1,this.t2);
-    	}else{
-    		return Diff(this.t2,this.t1);
-    	}
-    }
-    private int Diff(String y1,String y2) {
+    public int getDiff() {
     	int		res = 0;
-    	int[]	tmp = new int[6];
-    	String[]	fory1 = y1.split("\\D"),fory2 = y2.split("\\D");
-    	for (int i = 0;i < 3;i++){
-    		tmp[i*2]   = Integer.parseInt(fory1[i]);
-    		tmp[i*2+1] = Integer.parseInt(fory2[i]);
-    	}
-    	for (int i = 0;i<1+tmp[0]-tmp[1];i++)//所有整年的总天数天数
+    	for (int i = 0;i<1+this.temp[0]-this.temp[1];i++)//所有整年的总天数天数
         {
-            if((tmp[1]+i)%100 == 0){
-            	if((tmp[1]+i)%400 == 0){
-            		res = RN(i,tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5],res);
+            if((this.temp[1]+i)%100 == 0){
+            	if((this.temp[1]+i)%400 == 0){
+            		res = RN(i,this.temp[0],this.temp[1],this.temp[2],this.temp[3],this.temp[4],this.temp[5],res);
             	}else{
-            		res = FRN(i,tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5],res);
+            		res = FRN(i,this.temp[0],this.temp[1],this.temp[2],this.temp[3],this.temp[4],this.temp[5],res);
             	}
             }else{
-            	if((tmp[1]+i)%4 == 0){
-            		res = RN(i,tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5],res);
+            	if((this.temp[1]+i)%4 == 0){
+            		res = RN(i,this.temp[0],this.temp[1],this.temp[2],this.temp[3],this.temp[4],this.temp[5],res);
             	}else{
-            		res = FRN(i,tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5],res);
+            		res = FRN(i,this.temp[0],this.temp[1],this.temp[2],this.temp[3],this.temp[4],this.temp[5],res);
             	}
             }
         }
-	return res;
+		return Math.abs(res);
     }
 	private int RN(int i,int temp1, int temp2, int temp3, int temp4, int temp5,
 			int temp6,int res) {
@@ -104,7 +115,8 @@ public class countTime{
 		double[][]	Ce	= {
 				{4.63,19.46,6.38,21.416,5.59,20.888,6.318,21.86,6.500,22.20,7.928,23.65,8.35,23.95,8.440,23.822,9.098,24.218,8.218,23.08,7.90,22.60,6.1100,20.84},
 				{3.87,18.74,5.63,20.646,4.81,20.100,5.520,21.04,5.678,21.37,7.108,22.83,7.50,23.13,7.646,23.042,8.318,23.438,7.438,22.36,7.18,21.94,5.4055,20.12},
-				{4.15,18.73,5.63,20.646,5.59,20.888,6.318,21.86,6.500,22.20,7.928,23.65,8.35,23.95,8.440,23.822,9.098,24.218,8.218,23.08,7.90,22.60,6.1100,20.84}};
+				{4.15,18.73,5.63,20.646,5.59,20.888,6.318,21.86,6.500,22.20,7.928,23.65,8.35,23.95,8.440,23.822,9.098,24.218,8.218,23.08,7.90,22.60,6.1100,20.84}
+				};
 		if (parseInt/100 >= 19){
 			if(i == 1 || i == 2){
 				if(Y == 0){
@@ -132,7 +144,6 @@ public class countTime{
 				JQDay[1]	= (int) (Y*D+Ce[1][(i*2+21)%24]-Y/4);
 			}
 		}
-		System.out.println(parseInt+"-"+i+":"+JQDay[0]+"/"+JQDay[1]);
 		return JQDay;
 	}
 }
